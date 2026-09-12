992fe0c85b986ac8ba5c6f7c3f41773d13a52360
premsen2703-prog <prem.sen.2703@gmail.com>
2026-09-12 15:27:05 +0530

    Delete calculate_total_and_percentage_of_marks.py

--- calculate_total_and_percentage_of_marks.py
+++
@@ -1,7 +0,0 @@
-subject1 = float(input("enter marks of subject1: "))
-subject2 = float(input("enter marks of subject2: "))
-subject3 = float(input("enter marks of subject3: "))
-total = subject3 + subject2 + subject1
-percentage = (total/ 300) * 100
-print(f"Total of marks: {total}")
-print(f"percentage of marks = {percentage:.2f}%")
